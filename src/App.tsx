dc5a5fb172c17a95a99384a691272024648e7a31
mcmikey <kamil.rahmatullin@gmail.com>
2024-10-22 20:51:16 +0300

    more things

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import './App.css'
 
 interface ItemProps {
   id: number,
   name: string,
+  search: string,
   listid: number,
   priority: number,
   setPriority(priority: number, id: number): void,
@@ -11,19 +12,33 @@ interface ItemProps {
 }
 
 function Item(props: ItemProps) {
+  const searched = useRef(false)
+
+  if (props.name.toLowerCase().includes(props.search) && props.search != '') {
+    searched.current = true
+  } else {
+    searched.current = false
+  }
 
   function handleCounter(e: any, id: number) {
-    props.setPriority(e.target.value, id)
+    if (e.target.value > 5) {
+      props.setPriority(5, id)
+    } 
+    else if (e.target.value < -5) {
+      props.setPriority(-5, id)
+    } else {
+      props.setPriority(e.target.value, id)
+    }
+    
   }
 
   return (
-    <div className="student">
+    <div className={searched.current ? 'student searched' : 'student'}>
       <div className="namecont">
         <p className="name">{String(props.listid) + ') ' + props.name}</p>
       </div>
       <div className="inputcont">
-        <label htmlFor="priority">Priority</label>
-        <input min={-5} max={5} onChange={(e) => handleCounter(e, props.id)} value={props.priority} className='priorinput' name='priority' type="number"/>
+        <input min={-5} max={5} onChange={(e) => handleCounter(e, props.id)} value={props.priority} className='priorinput' type="number"/>
         <button onClick={() => props.removeItem(props.id)} className="delete">Del</button>
       </div>
     </div>
@@ -33,41 +48,62 @@ function Item(props: ItemProps) {
 function App() {
 
   let group = [
-    [{id: 1, name: 'a', prior: 0}],
-    [{id: 2, name: 'b', prior: 0}],
-    [{id: 3, name: 'c', prior: 0}],
-    [{id: 4, name: 'd', prior: 0}],
-    [{id: 5, name: 'e', prior: 0}],
-    [{id: 6, name: 'f', prior: 0}],
-    [{id: 7, name: 'i', prior: 0}],
-    [{id: 8, name: 'j', prior: 0}],
-    [{id: 9, name: 'k', prior: 0}],
-    [{id: 10, name: 'l', prior: 0}],
-    [{id: 11, name: 'm', prior: 0}],
-    [{id: 12, name: 'n', prior: 0}],
-    [{id: 13, name: 'o', prior: 0}],
-    [{id: 14, name: 'p', prior: 0}],
-    [{id: 15, name: 'r', prior: 0}],
-    [{id: 16, name: 'a', prior: 0}],
-    [{id: 17, name: 'b', prior: 0}],
-    [{id: 18, name: 'c', prior: 0}],
-    [{id: 19, name: 'd', prior: 0}],
-    [{id: 20, name: 'e', prior: 0}],
-    [{id: 21, name: 'f', prior: 0}],
-    [{id: 22, name: 'i', prior: 0}],
-    [{id: 23, name: 'j', prior: 0}],
-    [{id: 24, name: 'k', prior: 0}],
-    [{id: 25, name: 'l', prior: 0}],
-    [{id: 26, name: 'm', prior: 0}],
-    [{id: 27, name: 'n', prior: 0}],
-    [{id: 28, name: 'o', prior: 0}],
-    [{id: 29, name: 'p', prior: 0}],
-    [{id: 30, name: 'r', prior: 0}],
-    [{id: 31, name: 'r', prior: 0}],
+    [{id: 1, name: '1', prior: 0}],
+    [{id: 2, name: '2', prior: 0}],
+    [{id: 3, name: '3', prior: 0}],
+    [{id: 4, name: '4', prior: 0}],
+    [{id: 5, name: '5', prior: 0}],
+    [{id: 6, name: 'Рахматуллин Камиль', prior: 0}],
+    [{id: 7, name: '6', prior: 0}],
+    [{id: 8, name: '7', prior: 0}],
+    [{id: 9, name: '8', prior: 0}],
+    [{id: 10, name: '9', prior: 0}],
+    [{id: 11, name: '10', prior: 0}],
+    [{id: 12, name: '11', prior: 0}],
+    [{id: 13, name: '12', prior: 0}],
+    [{id: 14, name: '13', prior: 0}],
+    [{id: 15, name: '14', prior: 0}],
+    [{id: 16, name: '15', prior: 0}],
+    [{id: 17, name: '16', prior: 0}],
+    [{id: 18, name: '17', prior: 0}],
+    [{id: 19, name: '19', prior: 0}],
+    [{id: 20, name: '19', prior: 0}],
+    [{id: 21, name: '20', prior: 0}],
+    [{id: 22, name: '21', prior: 0}],
+    [{id: 23, name: '22', prior: 0}],
+    [{id: 24, name: '23', prior: 0}],
+    [{id: 25, name: '24', prior: 0}],
+    [{id: 26, name: '25', prior: 0}],
+    [{id: 27, name: '26', prior: 0}],
+    [{id: 28, name: '27', prior: 0}],
+    [{id: 29, name: '28', prior: 0}],
+    [{id: 30, name: '29', prior: 0}],
+    [{id: 31, name: '30', prior: 0}],
   ]
   const [randlist, setRandlist] = useState(group)
   const [edit, setEdit] = useState('Default Table Name')
   const [editing, setEditing] = useState(false)
+  const [searching, setSearching] = useState('')
+
+  function quickSort(arr: number[]): number[] {
+    if (arr.length <= 1) {
+      return arr;
+    }
+  
+    const pivot = arr[arr.length - 1];
+    const leftArr = [];
+    const rightArr = [];
+  
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i] < pivot) {
+        leftArr.push(arr[i]);
+      } else {
+        rightArr.push(arr[i]);
+      }
+    }
+  
+    return [...quickSort(leftArr), pivot, ...quickSort(rightArr)];
+  }
 
   function shuffle(array: any) {
     let copyarr = array.slice()
@@ -89,15 +125,16 @@ function App() {
         subs[randlist[i][0].prior].push(randlist[i][0])
       }
     }
-    let keys: (string | number)[] = Object.keys(subs)
+    let keys: (string)[] = Object.keys(subs)
+    let numkeys: number[] = []
     for (let i = 0; i < keys.length; i++) {
-      keys[i] = Number(keys[i])
+      numkeys.push(Number(keys[i]))
     }
-    keys.sort()
-    keys.reverse()
+    numkeys = quickSort(numkeys)
+    numkeys.reverse()
     let p_shuffle: any = []
-    for (let i = 0; i < keys.length; i++) {
-      let sub = shuffle([subs[keys[i]]][0])
+    for (let i = 0; i < numkeys.length; i++) {
+      let sub = shuffle([subs[numkeys[i]]][0])
       p_shuffle.push(sub)
     }
     let newarr: any = []
@@ -111,7 +148,6 @@ function App() {
 
   function removeItem(id: number) {
     let copyarr = randlist.slice()
-    console.log()
     for (let i = 0; i < randlist.length; i++) {
       if (randlist[i][0].id == id) {
         copyarr.splice(i, 1)
@@ -132,22 +168,12 @@ function App() {
 
   function handleShuffle() {
     subgroup()
-    // if (!localStorage.getItem('reshuffles')) {
-    //   localStorage.
-    //   localStorage.setItem('reshuffles', '1')
-    // } else {
-    //   let num = Number(localStorage.getItem('reshuffles')) + 1
-    //   localStorage.setItem('reshuffles', String(num))
-    //   let temp = localStorage.getItem('reshuffles')
-    //   setReshuffles(temp)
-    // }
     if (!localStorage.getItem('test')) {
       localStorage.setItem('test', '1')
     } else {
       let temp = Number(localStorage.getItem('test')) + 1
       localStorage.setItem('test', String(temp))
     }
-    console.log(localStorage.getItem('test'))
   }
   function handleReset() {
     for (let i = 0; i < group.length; i++) {
@@ -156,6 +182,7 @@ function App() {
     setRandlist(group)
     setEdit('Default Table Name')
     setEditing(false)
+    setSearching('')
   }
   function handleEdit(e: any) {
     setEdit(e.target.value)
@@ -163,24 +190,39 @@ function App() {
   function handleEditing() {
     setEditing(!editing)
   }
+  function handleSearch(e: any) {
+    setSearching(e.target.value)
+  }
+
   return (
     <>
     <div className="cont">
+      <p className="reshuffles">Reshuffles on this device: {localStorage.getItem('test')}</p>
       <div className="btns">
         <button onClick={() => handleReset()} className="reset">Reset</button>
         <button onClick={() => handleShuffle()} className="shuffle">Shuffle</button>
       </div>
-      <p className="reshuffles">Reshuffles on this device: {localStorage.getItem('test')}</p>
+      <div className="searchcont">
+        <input onChange={(e) => handleSearch(e)} type="text" className="search" placeholder='Search for a student'/>
+      </div>
       <div className="items">
         <div className="editcont">
           {editing ? 
-          <input value={edit} onChange={(e) => handleEdit(e)} type="text" className="tablename" placeholder='Table Name'/> : 
+          <input maxLength={30} value={edit} onChange={(e) => handleEdit(e)} type="text" className="tablename" placeholder='Table Name'/> : 
           <p className="shufflename">{edit}</p>}
           <button onClick={() => handleEditing()} className="editbtn delete">Edit</button>
         </div>
         {randlist.map((i) => {
           return (
-            <Item removeItem={removeItem} setPriority={setPriority} priority={i[0].prior} listid={randlist.indexOf(i) + 1} id={i[0].id} key={i[0].id} name={i[0].name}/>
+            <Item 
+            search={searching}
+            removeItem={removeItem} 
+            setPriority={setPriority} 
+            priority={i[0].prior} 
+            listid={randlist.indexOf(i) + 1} 
+            id={i[0].id} 
+            key={i[0].id} 
+            name={i[0].name}/>
           )
         })}
       </div>
