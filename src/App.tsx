ff810264ce2b6a192ec67920b6cdb2a63cbaacb6
mcmikey <kamil.rahmatullin@gmail.com>
2024-10-25 18:55:19 +0300

    hehehehe

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,13 @@ interface ItemProps {
   search: string,
   listid: number,
   priority: number,
-  setPriority(priority: number, id: number): void,
+  setPriority(priority: number | string, id: number): void,
   removeItem(id: number): void
 }
 
 function Item(props: ItemProps) {
   const searched = useRef(false)
-  const [priorinput, setPriorinput]: any = useState(props.priority)
+
   if (props.name.toLowerCase().includes(props.search) && props.search != '') {
     searched.current = true
   } else {
@@ -22,19 +22,17 @@ function Item(props: ItemProps) {
 
   function handleCounter(e: any, id: number) {
     props.setPriority(e.target.value, id)
-    setPriorinput(e.target.value)
   }
   function handleBlur(e: any, id: number) {
     if (e.target.value == '') {
       props.setPriority(0, id)
-      setPriorinput(0)
     } else {
       props.setPriority(Number(e.target.value), id)
     }
   }
-  function handleFocus(e: any) {
+  function handleFocus(e: any, id: number) {
     if (e.target.value == 0) {
-      setPriorinput('')
+      props.setPriority('', id)
     }
   }
   return (
@@ -44,7 +42,7 @@ function Item(props: ItemProps) {
       </div>
       <div className="inputcont">
         <label htmlFor="prior">Приоритет</label>
-        <input name='prior' onFocus={(e) => handleFocus(e)} onBlur={(e) => handleBlur(e, props.id)} onChange={(e) => handleCounter(e, props.id)} value={priorinput} className='priorinput' type="number"/>
+        <input name='prior' onFocus={(e) => handleFocus(e, props.id)} onBlur={(e) => handleBlur(e, props.id)} onChange={(e) => handleCounter(e, props.id)} value={props.priority} className='priorinput' type="number"/>
         <button onClick={() => props.removeItem(props.id)} className="delete">
         <svg className='binsvg' fill="#000000" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 408.483 408.483">
         <g>
@@ -181,7 +179,7 @@ function App() {
     setRandlist(copyarr)
   }
 
-  function setPriority(priority: number, id: number) {
+  function setPriority(priority: any, id: number) {
     let copyarr = randlist.slice()
     for (let i = 0; i < copyarr.length; i++) {
       if (copyarr[i][0].id == id) {
@@ -248,7 +246,7 @@ function App() {
           <button onClick={() => handleEditing()} className="editbtn delete">
 <svg className='editsvg' viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M21.2799 6.40005L11.7399 15.94C10.7899 16.89 7.96987 17.33 7.33987 16.7C6.70987 16.07 7.13987 13.25 8.08987 12.3L17.6399 2.75002C17.8754 2.49308 18.1605 2.28654 18.4781 2.14284C18.7956 1.99914 19.139 1.92124 19.4875 1.9139C19.8359 1.90657 20.1823 1.96991 20.5056 2.10012C20.8289 2.23033 21.1225 2.42473 21.3686 2.67153C21.6147 2.91833 21.8083 3.21243 21.9376 3.53609C22.0669 3.85976 22.1294 4.20626 22.1211 4.55471C22.1128 4.90316 22.0339 5.24635 21.8894 5.5635C21.7448 5.88065 21.5375 6.16524 21.2799 6.40005V6.40005Z" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M11 4H6C4.93913 4 3.92178 4.42142 3.17163 5.17157C2.42149 5.92172 2 6.93913 2 8V18C2 19.0609 2.42149 20.0783 3.17163 20.8284C3.92178 21.5786 4.93913 22 6 22H17C19.21 22 20 20.2 20 18V13" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M11 4H6C4.93913 4 3.92178 4.42142 3.17163 5.17157C2.42149 5.92172 2 6.93913 2 8V18C2 19.0609 2.42149 20.0783 3.17163 20.8284C3.92178 21.5786 4.93913 22 6 22H17C19.21 22 20 20.2 20 18V13" stroke="#000000" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
 </svg>
           </button>
         </div>
